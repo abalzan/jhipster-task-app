23c9c526796bae96b311ec9047ca5f7206b4ae50
Andrei <andreibalzan@gmail.com>
2017-11-28 17:03:46 +0100

    Make tasks visible in home page

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,7 @@
+import { TaskService } from '../entities/task/task.service';
+import { Task } from '../entities/task/task.model';
+import { ResponseWrapper } from '../shared/model/response-wrapper.model';
 import { Component, OnInit } from '@angular/core';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
-
-import { Account, LoginModalService, Principal } from '../shared';
 
 @Component({
     selector: 'jhi-home',
@@ -13,36 +12,20 @@ import { Account, LoginModalService, Principal } from '../shared';
 
 })
 export class HomeComponent implements OnInit {
-    account: Account;
-    modalRef: NgbModalRef;
 
-    constructor(
-        private principal: Principal,
-        private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
-    ) {
+  tasks: Task[] = [];
+  
+  constructor(private taskService: TaskService){ }
+    
+    loadAll() {
+        this.taskService.query().subscribe(
+            (res: ResponseWrapper) => {
+                this.tasks = res.json;
+            },
+            (res: ResponseWrapper) => console.log(res.json)
+        );
     }
-
     ngOnInit() {
-        this.principal.identity().then((account) => {
-            this.account = account;
-        });
-        this.registerAuthenticationSuccess();
-    }
-
-    registerAuthenticationSuccess() {
-        this.eventManager.subscribe('authenticationSuccess', (message) => {
-            this.principal.identity().then((account) => {
-                this.account = account;
-            });
-        });
-    }
-
-    isAuthenticated() {
-        return this.principal.isAuthenticated();
-    }
-
-    login() {
-        this.modalRef = this.loginModalService.open();
+        this.loadAll();
     }
 }
